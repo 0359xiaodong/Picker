d17066325591e2dee997052456f699f431ca6585
tubingbing <tubingbing@tubingbingdeMacBook-Pro.local>
2015-10-12 09:33:51 +0800

    upload to bintray

--- app/src/main/java/com/tubb/picker/MainActivity.java
+++ app/src/main/java/com/tubb/picker/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvInfo = (TextView)findViewById(R.id.tvInfo);
+        Sw
     }
 
     public void selectDate(View view){
